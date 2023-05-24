f03f16cfe54ca334b93fe38c9feaecdf88bf285c
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-05-24 15:49:11 +0600

    addEnv

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const app = express();
 const port = 3000;
 
 // Create MySQL connection
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'your_username',
-//   password: 'your_password',
-//   database: 'your_database_name'
-// });
+const db = mysql.createConnection({
+  host: process.env.LOCALHOST,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.Database_Name
+});
 
 // Connect to the database
-// db.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected to MySQL database');
-// });
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
